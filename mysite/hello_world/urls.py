1292e956a63996e4d0c3557f65a6b46019183209
jiahLinn <ngwe2.linn@live.uwe.ac.uk>
2024-02-07 12:50:19 +0000

    home page and bootstrap

--- mysite/hello_world/urls.py
+++ mysite/hello_world/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from hello_world import views
 
 urlpatterns = [
-    path("", views.hello_world, name="hello_world"),
+    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("hello_world", views.hello_world, name="hello_world"),
 ]
--- mysite/hello_world/views.py
+++ mysite/hello_world/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, "index.html", {})
 
 def hello_world(request):
     return render(request, "hello_world.html", {})
